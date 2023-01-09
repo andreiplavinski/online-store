614df3fac31d7eb5c5b1ebd1755f26815bdb272c
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-01-09 22:14:32 +0300

    feat: add rewrite filters after reload

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,17 @@ import "./assets/images/visacard.png";
 import "./assets/images/amexcard.png";
 import "./assets/images/mastercard.png";
 import App from "./pages/app";
+import SortProducts from "./pages/products/sortProducts";
 
 const app = new App();
 app.run();
+
+window.addEventListener("load", () => {
+  const a = document.querySelector(".catalog");
+  const b = document.querySelector(".filter");
+  if (a instanceof HTMLElement && b instanceof HTMLElement) {
+    const write = new SortProducts(a, b);
+    write.writeResSearch();
+  }
+  console.log("hello");
+});
