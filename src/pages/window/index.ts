02930f1b5a2f611bd81ff6236052c23e831a0567
Andrei Plavinski <plavinskiandrei@gmail.com>
2023-01-11 17:59:54 +0300

    fix: fix email validation

--- src/pages/window/index.ts
+++ src/pages/window/index.ts
@@ -56,7 +56,7 @@ class window {
     personalEmail.className = "personal-wraper__email";
     personalEmail.type = "email";
     personalEmail.pattern =
-      "^((([0-9A-Za-z]{1}[-0-9A-z.]{0,30}[0-9A-Za-z]?)|([0-9А-Яа-я]{1}[-0-9А-я.]{0,30}[0-9А-Яа-я]?))@([-A-Za-z]{1,}.){1,}[-A-Za-z]{2,})$";
+      "^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$";
     personalEmail.placeholder = "E-mail";
     personalEmail.required = true;
 
